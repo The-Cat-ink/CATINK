74069c9cde489189d2093f1c6b562933ff7c565b
unknown <elobador@gmail.com>
2026-05-26 17:17:49 -0600

    Fix boton de editar paginas informativas

--- views/admin.php
+++ views/admin.php
@@ -102,7 +102,7 @@ function formatNumberShort($num){
     <!-- Botón para abrir el modal -->
      <?php if($superadmin): ?>
         <a href="paginas.php" class="btn btn-accent"><i class="bi bi-card-text"></i> Editar Páginas Informativas</a>
-        <button id="btnAbrirModal" class="btn btn-accent">
+        <button id="btnAbrirModal" class="btn btn-accent" style="font-weight: 525;">
             <i class="bi bi-gear"></i> Gestionar Estado de Secciones
         </button>
     <?php endif; ?>
