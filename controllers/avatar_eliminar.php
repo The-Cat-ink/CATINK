43845f3bb6b16f76e0a04d471f209fb15d979dbc
unknown <elobador@gmail.com>
2026-06-02 14:46:01 -0600

    Fix: Cambiar rutas de avatares de img/ a uploads/

--- controllers/avatar_eliminar.php
+++ controllers/avatar_eliminar.php
@@ -20,9 +20,9 @@ if($row){
     // Manejo de rutas: si es solo el nombre, agregar ruta; si es ruta completa, usar como está
     $rutaImagen = $row['imagen'];
     if(strpos($rutaImagen, '/') === false){
-        $rutaImagen = 'img/avatares/' . $rutaImagen;
+        $rutaImagen = 'uploads/avatares/' . $rutaImagen;
     }
-    $archivo = __DIR__ . '/../' . $rutaImagen;
+    $archivo = dirname(__DIR__) . '/' . $rutaImagen;
     if(file_exists($archivo)) unlink($archivo);
 
     $stmt = $con->prepare("DELETE FROM avatares_perfil WHERE id_avatar = ?");
