7205d3bf01780886bc0a343e11eb12a67ad08b19
4strob0y <elobador@gmail.com>
2026-07-24 13:28:33 -0600

    Fix: Usar credenciales SMTP_AUTH (no-reply) para formulario de contacto para evitar filtro antispam de Hostinger

--- controllers/contacto_enviar.php
+++ controllers/contacto_enviar.php
@@ -38,14 +38,14 @@ try {
     $mail->isSMTP();
     $mail->Host       = env('SMTP_HOST', 'smtp.hostinger.com');
     $mail->SMTPAuth   = true;
-    $mail->Username   = env('SMTP_USERNAME', env('SMTP_FROM_EMAIL'));
-    $mail->Password   = env('SMTP_PASSWORD', '');
+    $mail->Username   = env('SMTP_AUTH_USERNAME', env('SMTP_USERNAME', env('SMTP_FROM_EMAIL')));
+    $mail->Password   = env('SMTP_AUTH_PASSWORD', env('SMTP_PASSWORD', ''));
     $mail->SMTPSecure = env('SMTP_SECURE', 'ssl');
     $mail->Port       = intval(env('SMTP_PORT', 465));
     $mail->CharSet    = 'UTF-8';
 
-    $fromEmail = env('SMTP_FROM_EMAIL', 'no-reply@catink.com.mx');
-    $fromName  = env('SMTP_FROM_NAME',  'CatInk');
+    $fromEmail = env('SMTP_AUTH_FROM_EMAIL', env('SMTP_FROM_EMAIL', 'no-reply@catink.com.mx'));
+    $fromName  = env('SMTP_AUTH_FROM_NAME',  env('SMTP_FROM_NAME',  'CatInk'));
 
     $mail->setFrom($fromEmail, $fromName);
     $mail->addAddress('contacto@catink.com.mx', 'CatInk Contacto');
